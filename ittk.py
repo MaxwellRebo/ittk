82e04a87a318f85090321ff11f9116d43ed2546b
Maxwell Rebo <maxwell.b.rebo@gmail.com>
2015-01-03 19:00:31 -0500

    failing tests

--- ittk.py
+++ ittk.py
@@ -4,29 +4,32 @@ Ittk: Information Theory Toolkit.
 MIT license.
 '''
 
-#All default to log base 2.  Modify to your purposes.
 from __future__ import division
+
+import unittest
 import math
 import numpy as np
 import ittk_helpers as hlp
-from hlp import probs
+from ittk_helpers import probs
 from numpy import array, shape, where, in1d
+
+# All functions default to log base 2.
 
 
 def entropy(X):
     X = probs(X)
     total = 0
     for x in X:
-        if x==0: continue
-        total -= x*np.log2(x)
+        if x == 0:
+            continue
+        total -= x * np.log2(x)
     return total
 
-def mutualInformation(X, Y, normalized=False, base=2):
-    #Accepts lists or numpy arrays; will make X and Y into numpy arrays if they're not already
-    if (type(X).__module__ !='numpy'):
-        X = array(X)
-    if (type(Y).__module__ !='numpy'):
-        X = array(Y)
+
+# Accepts lists or numpy arrays; will make X and Y into numpy arrays if they're not already
+def mutual_information(X, Y, normalized=False, base=2):
+    X = hlp.check_numpy_array(X)
+    Y = hlp.check_numpy_array(Y)
     numobs = len(X)
     if numobs != len(Y):
         raise Exception("Not matching length")
@@ -36,21 +39,23 @@ def mutualInformation(X, Y, normalized=False, base=2):
     uniq_y = set(Y)
     for x in uniq_x:
         for y in uniq_y:
-            px = shape(where(X==x))[1] / numobs
-            py = shape(where(Y==y))[1] / numobs
-            pxy = len(where(in1d(where(X==x)[0], 
-                            where(Y==y)[0])==True)[0]) / numobs
+            px = shape(where(X == x))[1] / numobs
+            py = shape(where(Y == y))[1] / numobs
+            pxy = len(where(in1d(where(X == x)[0],
+                                 where(Y == y)[0]) == True)[0]) / numobs
             if pxy > 0.0:
-                mutual_info += pxy * math.log((pxy / (px*py)), base)
-    if normalized: mutual_info = mutual_info / np.log2(numobs) 
+                mutual_info += pxy * math.log((pxy / (px * py)), base)
+    if normalized: mutual_info = mutual_info / np.log2(numobs)
     return mutual_info
 
-#Variation of information
-def informationVariation(X, Y):
-    return entropy(X) + entropy(Y) - 2*mutualInformation(X, Y)
+
+# Variation of information
+def information_variation(X, Y):
+    return entropy(X) + entropy(Y) - (2 * mutual_information(X, Y))
+
 
 def kldiv(X, Y, isprobs=False):
-    if isprobs==False:
+    if isprobs == False:
         p = probs(X)
         q = probs(Y)
     else:
@@ -59,19 +64,52 @@ def kldiv(X, Y, isprobs=False):
     p, q = hlp.match_arrays(p, q)
     logpq = np.array([])
     for i in range(len(p)):
-        if q[i]==0 or p[i]==0: logpq = np.append(logpq, 0)
-        else: logpq = np.append(logpq, np.log2(p[i]/q[i]))
-    kldivergence = np.dot( p, logpq )
+        if q[i] == 0 or p[i] == 0:
+            logpq = np.append(logpq, 0)
+        else:
+            logpq = np.append(logpq, np.log2(p[i] / q[i]))
+    kldivergence = np.dot(p, logpq)
     return kldivergence
 
-#Note: this will reduce the length of the sequence by the number of lag points
+
+# Note: this will reduce the length of the sequence by the number of lag points
 #X: numpy array
 #Y: numpy array
-#lag: integer.  defaults to 1
-def laggedMutualInformation(X, Y, lag=1):
-    for i in range(lag):
-        X.pop(0)
-        Y.pop()
-    return mutualInformation(X, Y)
+#lag_points: integer. defaults to 1
+def lag(x, y, lag_points=1):
+    for i in range(lag_points):
+        x.pop(0)
+        y.pop()
+    return x, y
+
+
+def lagged_mutual_information(X, Y, lag_points=1):
+    X, Y = lag(X, Y, lag_points)
+    return mutual_information(X, Y)
+
+
+class TestMutualInformation(unittest.TestCase):
+    def test_mutual_information(self):
+        x = np.array([7, 7, 7, 3])
+        y = np.array([0, 1, 2, 3])
+        mut_inf = mutual_information(x, y)
+        self.assertEquals(0.8112781244591329, mut_inf)
+
+
+class TestInformationVariation(unittest.TestCase):
+    def test_information_variation(self):
+        x = np.array([7, 7, 7, 3])
+        y = np.array([0, 1, 2, 3])
+        inf_var = information_variation(x, y)
+        self.assertEquals(1.1887218755408671, inf_var)
+
+
+class TestProbs(unittest.TestCase):
+    def test_mutual_information(self):
+        self.assertEquals(hlp.probs(probs([1,2,3])), array([ 0.,  0.33333333,  0.33333333,  0.33333333]))
+
+
+if __name__ == '__main__':
+    unittest.main()
 
 
--- ittk_helpers.py
+++ ittk_helpers.py
@@ -1,10 +1,12 @@
 import numpy as np
 
+
 def probs(X):
     n = len(X)
     c = np.bincount(X)
-    P = c / float(n)
-    return P
+    p = c / float(n)
+    return p
+
 
 def cond_probs(obs):
     if type(obs) is str:
@@ -22,15 +24,16 @@ def cond_probs(obs):
             counts[sym][other_sym] = 0
             probs_dict[sym][other_sym] = 0
     for i in range(len(obs) - 1):
-        if obs[i+1] in counts[obs[i]]:
-            counts[obs[i]][obs[i+1]] += 1
+        if obs[i + 1] in counts[obs[i]]:
+            counts[obs[i]][obs[i + 1]] += 1
             continue
-        counts[obs[i]][obs[i+1]] = 1
+        counts[obs[i]][obs[i + 1]] = 1
     for sym in syms:
         div = 1 if totals[sym] is 0 else totals[sym]
         for other_sym in syms:
             probs_dict[sym][other_sym] = counts[sym][other_sym] / float(div)
     return probs_dict
+
 
 def match_arrays(X, Y):
     if len(X) > len(Y):
@@ -41,15 +44,24 @@ def match_arrays(X, Y):
             X = np.append(X, 0)
     return (X, Y)
 
+
+def check_numpy_array(x):
+    if type(x).__module__ != 'numpy':
+        x = np.array(x)
+    return x
+
+
 # Number of unique symbols in list/array
 def num_unique(X):
-	return len(set(X))
+    return len(set(X))
+
 
 # Checks that number of unique symbols in arrays is the same
 def bin_match(X, Y):
-	return num_unique(X) == num_unique(Y)
+    return num_unique(X) == num_unique(Y)
+
 
 # Verifies that observation variables of X and Y are the same, i.e. they take values in the same probability space
 def variable_match(X, Y):
-	return set(X) == set(Y)
+    return set(X) == set(Y)
 
