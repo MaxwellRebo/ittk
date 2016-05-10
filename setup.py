39bc07f783e0b3aed3119eaac810056008c1d328
Maxwell Rebo <maxwell.b.rebo@gmail.com>
2016-05-09 23:24:27 -0400

    setup script

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import os
+import sys
+from setuptools import setup, find_packages
+
+if sys.argv[-1] == 'install':
+    setup(name='ittk',
+          version='1.0.0',
+          description='Information Theory Toolkit',
+          author='Maxwell Rebo',
+          author_email='maxwell.b.rebo@gmail.com',
+          url='https://github.com/MaxwellRebo/ittk',
+          packages=find_packages()
+          )
